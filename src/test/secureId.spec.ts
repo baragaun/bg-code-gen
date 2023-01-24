9f037dbd6c3992c288731d7dcbe0922f9eb92153
shadowbrush <github@shadowbrush.com>
2023-01-24 10:58:02 -0800

    Fixed test name

--- src/test/secureId.spec.ts
+++ src/test/secureId.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import codeGen from '../index.js'
 import config from '../projects/secureId/config.js'
 
-describe('Quark', () => {
+describe('SecureId', () => {
   it('runs the code generator for SecureId', async () => {
     const result = await codeGen(config)
     expect(result).to.equal(0)
